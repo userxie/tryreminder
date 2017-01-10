ca723bf3dff3fa4dea2fd98090cf5edb6ad6bde5
xcf <384207719@qq.com>
2017-01-10 21:08:18 +0800

    Adds ListView with custom colors

--- app/src/main/java/com/tryreminder/myandroid/newreminder/ReminderActivity.java
+++ app/src/main/java/com/tryreminder/myandroid/newreminder/ReminderActivity.java
@@ -8,45 +8,25 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ReminderActivity extends AppCompatActivity {
 
+    private  ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reminder);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_reminders);
+        mListView =(ListView)findViewById(R.id.reminder_list_view);
+        ArrayAdapter<String> arrayAdapter =new ArrayAdapter<String>(
+                this,
+                R.layout.reminders_row,
+                R.id.row_text,
+                new String[]{"first record","second record","third record"});
+        mListView.setAdapter(arrayAdapter);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_reminder, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
